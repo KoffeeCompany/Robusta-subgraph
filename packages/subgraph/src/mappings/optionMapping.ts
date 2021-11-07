24c28cd10b7901eac5843df167a4c5ef0ea287ad
andryrkt1 <34197445+andryrkt1@users.noreply.github.com>
2021-11-07 12:17:38 +0100

    docker it

---
+++ packages/subgraph/src/mappings/optionMapping.ts
@@ -0,0 +1,18 @@
+import { log } from "@graphprotocol/graph-ts";
+import { Option } from "../../generated/Option/Option";
+import {
+  LogOptionCreation,
+  LogSettle,
+  LogCancel,
+} from "../../generated/Option/Option";
+import {
+  extractBigInt,
+  extractAddressFromBytes32,
+  extractBoolean,
+} from "../utils/helpers";
+
+export function handleLogCreateOption(event: LogOptionCreation): void {}
+
+export function handleLogSettle(event: LogSettle): void {}
+
+export function handleLogCancel(event: LogCancel): void {}
